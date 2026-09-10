031e721290645fa0e707d55d28f35b66645fecc9
u <>
2026-09-10 15:27:52 +0900

    setup less and etckeeper

--- setup-arch.sh
+++ setup-arch.sh
@@ -2,12 +2,18 @@
 set -euo pipefail
 
 sudo pacman -Syu --needed base-devel \
-  git unzip jq ripgrep fd fzf trash-cli \
+  less unzip curl \
+  git jq ripgrep fd fzf trash-cli \
   python python-pip nodejs npm rustup \
   etckeeper
 
 ./setup-nvim.sh
 ./setup-go.sh
+
+# setup etckeeper
+sudo etckeeper init
+sudo sed -i 's/^#* *AVOID_DAILY_AUTOCOMMITS=.*/AVOID_DAILY_AUTOCOMMITS=1/' /etc/etckeeper/etckeeper.conf
+sudo systemctl mask --now etckeeper.timer
 
 echo ""
 echo "Finished. Please reboot."
