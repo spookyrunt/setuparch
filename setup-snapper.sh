a632a5a87f5f9a37185800dad17ec1c5d93ea582
spookyrunt <283396452+spookyrunt@users.noreply.github.com>
2026-09-11 13:36:37 +0900

    Reduce timeline and number limits in snapper config

--- setup-snapper.sh
+++ setup-snapper.sh
@@ -5,11 +5,24 @@ sudo pacman -S --needed --noconfirm snap-pac
 sudo snapper -c root set-config \
   TIMELINE_CREATE="yes" \
   TIMELINE_CLEANUP="yes" \
-  TIMELINE_LIMIT_HOURLY="2" \
-  TIMELINE_LIMIT_DAILY="2" \
-  TIMELINE_LIMIT_WEEKLY="2" \
+  TIMELINE_LIMIT_HOURLY="1" \
+  TIMELINE_LIMIT_DAILY="1" \
+  TIMELINE_LIMIT_WEEKLY="1" \
   TIMELINE_LIMIT_MONTHLY="1" \
   TIMELINE_LIMIT_YEARLY="0" \
   NUMBER_CLEANUP="yes" \
-  NUMBER_LIMIT="5" \
-  NUMBER_LIMIT_IMPORTANT="5"
+  NUMBER_LIMIT="4" \
+  NUMBER_LIMIT_IMPORTANT="4"
+sudo snapper -c home set-config \
+  TIMELINE_CREATE="yes" \
+  TIMELINE_CLEANUP="yes" \
+  TIMELINE_LIMIT_HOURLY="1" \
+  TIMELINE_LIMIT_DAILY="1" \
+  TIMELINE_LIMIT_WEEKLY="1" \
+  TIMELINE_LIMIT_MONTHLY="1" \
+  TIMELINE_LIMIT_YEARLY="0" \
+  NUMBER_CLEANUP="yes" \
+  NUMBER_LIMIT="4" \
+  NUMBER_LIMIT_IMPORTANT="4"
+sudo snapper -v -c root cleanup all
+sudo snapper -v -c home cleanup all
