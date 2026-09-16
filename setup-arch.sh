73751073b70fbdc9694a7e038933bcc7495c2ec7
spookyrunt <283396452+spookyrunt@users.noreply.github.com>
2026-09-17 02:19:36 +0900

    Set global git user for etckeeper setup
    Configure global git user for etckeeper and initialize.

--- setup-arch.sh
+++ setup-arch.sh
@@ -31,7 +31,9 @@ rm -rf /tmp/yay
 go install github.com/lemonade-command/lemonade@latest
 
 # setup etckeeper
+sudo git config --global user.name u
 sudo etckeeper init
+sudo etckeeper commit -m init
 sudo sed -i 's/^#* *AVOID_DAILY_AUTOCOMMITS=.*/AVOID_DAILY_AUTOCOMMITS=1/' /etc/etckeeper/etckeeper.conf
 sudo systemctl mask --now etckeeper.timer
 
